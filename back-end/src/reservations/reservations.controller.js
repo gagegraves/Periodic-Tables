bf8f118375d30121d70229874643bb1178686f96
Gage Graves <gagegraves97+github@gmail.com>
2021-10-15 15:48:30 -0700

    console logs added in backend to test validation for new reservation submission

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -74,12 +74,15 @@ async function validateReservationId(req, res, next) {
 //makes sure the data sent in from the request matches the restaurants rules for reservations
 function validateReservationDate(req, res, next) {
   const reservation = req.body.data;
-  const reserveDate = new Date(
-    `${reservation.reservation_date}T${reservation.reservation_time}:00.000`
-  );
+  console.log("~ reservation", reservation);
+  const reserveDate = new Date(`${reservation.reservation_date}T${reservation.reservation_time}:00.000`);
+  console.log("~ reserveDate", reserveDate);
   const today = new Date();
+  console.log("~ today", today);
   const hours = reserveDate.getHours();
+  console.log("~ hours", hours);
   const mins = reserveDate.getMinutes();
+  console.log("~ mins", mins);
 
   if (reserveDate.getDay() === 2) {
     return next({
