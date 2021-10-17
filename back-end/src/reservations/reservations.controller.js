f638250d5957fdded00e0d9715a8a68d7c676a30
Gage Graves <gagegraves97+github@gmail.com>
2021-10-17 16:52:53 -0700

    added logs for server-side testing

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -79,7 +79,10 @@ async function validateReservationId(req, res, next) {
 //makes sure the data sent in from the request matches the restaurants rules for reservations
 async function validateReservationDate(req, res, next) {
 	const reserveDate = new Date(`${req.body.data.reservation_date}T${req.body.data.reservation_time}:00.000`);
+  console.log("~ reservation", req.body.data.reservation);
+  console.log("~ reserveDate", reserveDate);
 	const todaysDate = new Date();
+  console.log("~ todaysDate", todaysDate);
 
 	if(reserveDate.getDay() === 2) {  
 		return next({ status: 400, message: "'reservation_date' field: restauraunt is closed on tuesday" });
